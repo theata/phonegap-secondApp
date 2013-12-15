89699f403f33ae71b9bb8df35f2882120b9c781f
sillyboy <yyongtai@gmail.com>
2013-12-15 16:20:58 +0800

    remove alert message

--- js/main.js
+++ js/main.js
@@ -7,13 +7,7 @@ var app = {
         }); 
     },
 
-    showAlert: function(message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
+
 
     renderHomeView: function() {
         $('body').html(this.homeTpl());
